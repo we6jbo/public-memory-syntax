3407a24070756e19e60a6aee95496b5f9cf8f1c3
we6jbo <we6jbo@gmail.com>
2025-09-15 19:05:00 -0700

    Update vault.sh

--- vault.sh
+++ vault.sh
@@ -7,8 +7,8 @@ EXPECTED="But I expected the ai to say learn_about doug mccabe"
 printf "$PROMPT"
 read BODY
 send() {
-  printf "%s\n%s\n" "$PROMPT" "$ADDED" "$BODY" "$EXPECTED" | nc -N 127.0.0.1 4096 2>/dev/null \
-    || printf "%s\n%s\n" "$PROMPT" "$ADDED" "$BODY"  "$EXPECTED" | nc -q 1 127.0.0.1 4096 2>/dev/null \
+  printf "%s\n%s\n" "$PROMPT" "$ADDED" "$BODY" "$EXPECTED" | nc -N 100.96.165.217 4096 2>/dev/null \
+    || printf "%s\n%s\n" "$PROMPT" "$ADDED" "$BODY"  "$EXPECTED" | nc -q 100.96.165.217 4096 2>/dev/null \
     || printf "%s\n%s\n" "$PROMPT" "$ADDED" "$BODY" "$EXPECTED" 
 }
 
